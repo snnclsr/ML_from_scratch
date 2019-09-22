dadaa5f6b25660f39c18fcf232bfb6086fd27fb9
snnclsr <sinancalisir001@gmail.com>
2019-09-22 23:08:54 +0300

    cmap to utils

--- utils.py
+++ utils.py
@@ -1,12 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+
+cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
 
 
-def plot_data(X, y, s=25, cmap=None):
-    plt.scatter(X[:, 0], X[:, 1], c=y, edgecolors='k', cmap=cmap, s=s)
+def plot_data(X, y, s=25):
+    plt.scatter(X[:, 0], X[:, 1], c=y, edgecolors='k', cmap=cmap_bold, s=s)
 
 
-def plot_decision_boundary(model, X, y, cmap=None, alpha=0.6):
+def plot_decision_boundary(model, X, y, alpha=0.6):
     x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
     y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
 
@@ -17,7 +20,7 @@ def plot_decision_boundary(model, X, y, cmap=None, alpha=0.6):
     Z = Z.reshape(xx.shape)
 
     plt.figure()
-    plt.contourf(xx, yy, Z, cmap=cmap, alpha=alpha)
+    plt.contourf(xx, yy, Z, cmap=cmap_bold, alpha=alpha)
 
     plt.xlim(xx.min(), xx.max())
     plt.ylim(yy.min(), yy.max())
