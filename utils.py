9992c39925148d5c860646ebd8784900e9492ee1
snnclsr <sinancalisir001@gmail.com>
2019-09-22 22:52:20 +0300

    plotting functions to utils

---
+++ utils.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def plot_data(X, y, s=25, cmap=None):
+    plt.scatter(X[:, 0], X[:, 1], c=y, edgecolors='k', cmap=cmap, s=s)
+
+
+def plot_decision_boundary(model, X, y, cmap=None, alpha=0.6):
+    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+
+    xx, yy = np.meshgrid(np.linspace(x_min, x_max, 200),
+                         np.linspace(y_min, y_max, 200))
+
+    Z = model.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+
+    plt.figure()
+    plt.contourf(xx, yy, Z, cmap=cmap, alpha=alpha)
+
+    plt.xlim(xx.min(), xx.max())
+    plt.ylim(yy.min(), yy.max())
