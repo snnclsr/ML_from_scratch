945aa9937e75abf0cc5ebf518ce07fe4e4810c8a
snnclsr <sinancalisir001@gmail.com>
2019-09-24 23:23:10 +0300

    k_means and cmap option for plotting

--- utils.py
+++ utils.py
@@ -5,11 +5,11 @@ from matplotlib.colors import ListedColormap
 cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
 
 
-def plot_data(X, y, s=25):
-    plt.scatter(X[:, 0], X[:, 1], c=y, edgecolors='k', cmap=cmap_bold, s=s)
+def plot_data(X, y, s=25, cmap=cmap_bold):
+    plt.scatter(X[:, 0], X[:, 1], c=y, edgecolors='k', cmap=cmap, s=s)
 
 
-def plot_decision_boundary(model, X, y, alpha=0.6):
+def plot_decision_boundary(model, X, y, cmap=cmap_bold, alpha=0.6):
     x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
     y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
 
@@ -20,7 +20,7 @@ def plot_decision_boundary(model, X, y, alpha=0.6):
     Z = Z.reshape(xx.shape)
 
     plt.figure()
-    plt.contourf(xx, yy, Z, cmap=cmap_bold, alpha=alpha)
+    plt.contourf(xx, yy, Z, cmap=cmap, alpha=alpha)
 
     plt.xlim(xx.min(), xx.max())
     plt.ylim(yy.min(), yy.max())
